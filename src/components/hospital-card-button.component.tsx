85b0cec022b579b712de521dfa96344711fb634b
Chris Medykiewicz <crsmyz@gmail.com>
2019-07-29 11:31:38 -0400

    Update hospital-card-button.component.tsx

--- src/components/hospital-card-button.component.tsx
+++ src/components/hospital-card-button.component.tsx
@@ -1,1 +1,13 @@
 // hospital-card-button.component.tsx
+import React from 'react';
+import './hospital-card.css';
+
+export default function HospitalCardButton(props: any) {
+  return (
+        <a href={props && props.nearestHospital && props.nearestHospital.mapsUrl ? props.nearestHospital.mapsUrl : 'null'}>
+          <button className="h-c-button">
+            <span className="h-c-button-content">Directions</span>
+          </button>
+        </a>
+  );
+}
