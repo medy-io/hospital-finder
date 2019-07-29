868c00ed7e0c46a833962e2f0e2fab42cfe4f4fd
Chris Medykiewicz <crsmyz@gmail.com>
2019-07-29 11:33:52 -0400

    Update hospital-card-body.component.tsx

--- src/components/hospital-card-body.component.tsx
+++ src/components/hospital-card-body.component.tsx
@@ -1,1 +1,24 @@
 // hospital-card-body.component.tsx
+import React from 'react';
+import './hospital-card.css';
+
+export default function HospitalCardBody(props: any) {
+  return (
+      <div className="h-c-content">
+        <main>
+          <div>
+            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.name : 'null'}
+          </div>
+          <div>
+            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.address : 'null'}
+          </div>
+          <div>
+            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.phone : 'null'}
+          </div>
+          <div>
+            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.website : 'null'}
+          </div>
+        </main>
+      </div>
+  );
+}
