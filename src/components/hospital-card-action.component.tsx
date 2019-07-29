ca8396d92a81cc15d7eddc6ba6630bddc0e59bda
Chris Medykiewicz <crsmyz@gmail.com>
2019-07-29 11:38:38 -0400

    Update hospital-card-action.component.tsx

--- src/components/hospital-card-action.component.tsx
+++ src/components/hospital-card-action.component.tsx
@@ -1,1 +1,11 @@
 // hospital-card-action.component.tsx
+import React from 'react';
+import './hospital-card.css';
+
+export default function HospitalCardAction(props: any) {
+  return (
+      <div className="h-c-action">
+      </div>
+    </div>
+  );
+}
