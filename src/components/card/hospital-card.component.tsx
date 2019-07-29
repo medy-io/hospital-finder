177c9c829182ddfd35d55ba104b77b9d3ff64d88
crsmyz <crsmyz@gmail.com>
2019-07-28 21:47:44 -0400

    Updating style

--- src/components/card/hospital-card.component.tsx
+++ src/components/card/hospital-card.component.tsx
@@ -3,35 +3,34 @@ import './hospital-card.css';
 
 export default function HospitalCard(props: any) {
   return (
-    <div>
-      <header>
+    <div className="hospital-card">
+      <header className="h-c-header">
         <i>LOGO</i>
         <span>PA Hospital Finder</span>
       </header>
-      <main>
-        <div>
-          {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.name : 'null'}
-        </div>
-        <div>
-            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.address : 'null'}        
-        </div>
-        <div>
-            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.phone : 'null'}          
-        </div>
-        <div>
-          {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.website : 'null'}
-        </div>
-        <div>
-          <a href={props && props.nearestHospital && props.nearestHospital.mapsUrl ? props.nearestHospital.mapsUrl : 'null'}>  
-            <button className="card-button">
-              Directions
-            </button>
-          </a>
-          <button className="card-button">
-              911
+      <div className="h-c-content">
+        <main>
+          <div>
+            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.name : 'null'}
+          </div>
+          <div>
+            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.address : 'null'}
+          </div>
+          <div>
+            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.phone : 'null'}
+          </div>
+          <div>
+            {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.website : 'null'}
+          </div>
+        </main>
+      </div>
+      <div className="h-c-action">
+        <a href={props && props.nearestHospital && props.nearestHospital.mapsUrl ? props.nearestHospital.mapsUrl : 'null'}>
+          <button className="h-c-button">
+            <span className="h-c-button-content">Directions</span>
           </button>
-        </div>
-      </main>
+        </a>
+      </div>
     </div>
   );
 }
