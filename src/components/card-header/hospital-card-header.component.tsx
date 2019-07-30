d66d8133aebbb26b789cb7c085d297cc29ea5404
Chris Medykiewicz <crsmyz@gmail.com>
2019-07-30 10:36:12 -0400

    Update hospital-card-header.component.tsx

--- src/components/card-header/hospital-card-header.component.tsx
+++ src/components/card-header/hospital-card-header.component.tsx
@@ -5,8 +5,8 @@ import './../hospital-card/hospital-card.css';
 export default function HospitalCardHeader() {
   return (
     <header className="h-c-header">
-      <i>LOGO</i>
-      <span>PA Hospital Finder</span>
+      <span className="h-c-logo"><i>LOGO</i></span>
+      <span className="h-c-title">PA Hospital Finder</span>
     </header>
   );
 }
