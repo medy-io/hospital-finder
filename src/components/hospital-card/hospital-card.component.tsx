edfb00688f1e2838ccaf2706a47ebe7892c1a0d3
Chris Medykiewicz <crsmyz@gmail.com>
2019-07-31 12:47:46 -0400

    Update hospital-card.component.tsx

--- src/components/hospital-card/hospital-card.component.tsx
+++ src/components/hospital-card/hospital-card.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HospitalCardHeader from './../card-header/hospital-card-header.component';
 import HospitalCardBody from './../card-body/hospital-card-body.component';
 import HospitalCardAction from './../card-action/hospital-card-action.component';
-import './hospital-card.css';
+import './hospital-card.scss';
 
 export default function HospitalCard(props: any) {
   return (
