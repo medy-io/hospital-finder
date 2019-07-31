d8b7d393450e9b28fe928f1678ff55ae296a2657
Chris Medykiewicz <crsmyz@gmail.com>
2019-07-31 12:19:58 -0400

    Update hospital-card-body.component.tsx

--- src/components/card-body/hospital-card-body.component.tsx
+++ src/components/card-body/hospital-card-body.component.tsx
@@ -6,16 +6,16 @@ export default function HospitalCardBody(props: any) {
   return (
     <div className="h-c-content">
       <main>
-        <div className="">
+        <div className="h-c-hospital-name">
           {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.name : 'null'}
         </div>
-        <div>
+        <div className="h-c-hospital-address">
           {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.address : 'null'}
         </div>
-        <div>
+        <div className="h-c-hospital-phone">
           {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.phone : 'null'}
         </div>
-        <div>
+        <div className="h-c-hospital-website">
           {props && props.nearestHospital && props.nearestHospital.name ? props.nearestHospital.website : 'null'}
         </div>
       </main>
