221f79d532e5c0f086bb1ffe0aafe052ca02461c
Chris Medykiewicz <crsmyz@gmail.com>
2019-08-08 08:43:03 -0400

    Update hospital-card-header.component.tsx

--- src/components/card-header/hospital-card-header.component.tsx
+++ src/components/card-header/hospital-card-header.component.tsx
@@ -1,11 +1,13 @@
 // hospital-card-header.component.tsx
 import React from 'react';
 import './card-header.scss';
+import 'APP_NAME' from './../../../constants/global-constants.constant';
+import 'STATE_NAME' from './../../../constants/global-constants.constant';
 
 export default function HospitalCardHeader() {
   return (
     <header className="h-c-header">
-      <span className="h-c-title">PA Hospital Finder</span>
+      <span className="h-c-title">{STATE_NAME[42]} {APP_NAME}</span>
     </header>
   );
 }
