fb3b8e078890ac6179e26dd74db14635ac247aca
crsmyz <crsmyz@gmail.com>
2019-08-11 22:48:39 -0400

    updating hospital card with constants

--- src/components/card-header/hospital-card-header.component.tsx
+++ src/components/card-header/hospital-card-header.component.tsx
@@ -1,13 +1,13 @@
 // hospital-card-header.component.tsx
 import React from 'react';
 import './card-header.scss';
-import 'APP_NAME' from './../../../constants/global-constants.constant';
-import 'STATE_NAME' from './../../../constants/global-constants.constant';
+import { APP_NAME } from './../../constants/global-constants.constant';
+import { STATE_NAME } from './../../constants/global-constants.constant';
 
 export default function HospitalCardHeader() {
   return (
     <header className="h-c-header">
-      <span className="h-c-title">{STATE_NAME[42]} {APP_NAME}</span>
+      <span className="h-c-title">{STATE_NAME[44]} {APP_NAME}</span>
     </header>
   );
 }
